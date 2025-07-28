b068d90927b013d9f9d11ba36b797493e774f4cd
t-yamamoto-being <t-yamamoto@beingcorp.co.jp>
2025-07-28 09:27:30 +0000

    訳文に変数を渡すように修正。ロケールファイルの構成を修正。locale_controllerの処理をApplication_controllerへ集約。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
 
+  def change_locale
+    locale = params[:locale].to_s.strip.to_sym
+    session[:locale] = locale if I18n.available_locales.include?(locale)
+    redirect_back(fallback_location: root_path)
+  end
+
   private
 
   def set_locale
--- app/controllers/locale_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class LocaleController < ApplicationController
-  def change
-    locale = params[:locale].to_s.strip.to_sym
-    session[:locale] = locale if I18n.available_locales.include?(locale)
-    redirect_back(fallback_location: root_path)
-  end
-end
